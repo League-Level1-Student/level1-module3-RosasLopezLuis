2e812eb577b05ce4bb8ab13527c1f5d7f0855499
ilabstaff <ilabstaff@172.16.116.77>
2020-02-25 19:29:07 -0800

    athlete

---
+++ src/_01_intro_to_static/Athlete.java
@@ -0,0 +1,33 @@
+package _01_intro_to_static;
+
+public class Athlete {
+	static int nextBibNumber;
+    static String raceLocation = "New York";
+    static String raceStartTime = "9:00am";
+
+    String name;
+    int speed;
+    int bibNumber;
+
+    Athlete (String name, int speed, int bibNumber){
+        this.name = name;
+        this.speed = speed;
+    }
+
+    public static void main(String[] args) {
+    	Athlete.nextBibNumber = 1;
+    	Athlete.raceLocation = "Chicago";
+    	Athlete.raceStartTime = "8:15am";
+    	Athlete tob = new Athlete("Toby",5,1);
+    	Athlete jay = new Athlete("Jay",3,1);
+    	if(tob.speed > jay.speed) {
+    		
+    	}
+    	else
+    		
+    	System.out.println(tob.name + ", " + tob.bibNumber + ", " + Athlete.raceLocation);	
+    	System.out.println(jay.name + ", " + jay.bibNumber + ", " + Athlete.raceLocation);
+        //create two athletes
+        //print their names, bibNumbers, and the location of their race. 
+    }
+}
