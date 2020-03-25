fb89818dd0602c73f26f0d9ee59be8890cd04562
LuisL <LuisL@DESKTOP-JEOQDGO>
2020-03-24 19:33:52 -0700

    jukebox

--- src/_03_jukebox/Jukebox.java
+++ src/_03_jukebox/Jukebox.java
@@ -9,9 +9,13 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.net.URL;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
@@ -21,32 +25,43 @@ import javazoom.jl.player.advanced.AdvancedPlayer;
 /*   If you don't have javazoom.jar in your project, you can download it from here: http://bit.ly/javazoom
  *   Right click your project and add it as a JAR (Under Java Build Path > Libraries).*/
 
-public class Jukebox implements Runnable {
+public class Jukebox implements Runnable  {
 
     public void run() {
 
 		// 1. Find an mp3 on your computer or on the Internet.
     	
 		// 2. Create a Song object for that mp3
-    		Song song = new Song("https://freedownloads.last.fm/download/569264057/Get%2BGot.mp3");
-    		Song song2 = new Song("https://www.youtube.com/watch?v=Bt-brUAx3Uo");
-    		loadImage("Myron-Lyrics-Lil-Uzi-Vert.jpg");
-    		Jukebox juke = new Jukebox();
- 
+    	Song uzi = new Song("https://www.youtube.com/watch?v=Bt-brUAx3Uo");
+    	Song chief = new Song("https://www.youtube.com/watch?v=RdrC-FrtwMI");
+    	Song tyler = new Song("https://www.youtube.com/watch?v=FUXX55WqYZs");
 		// 3. Play the Song
+    	uzi.play();
     	
-    	song.play();
-    		
+    
 		/*
-		 * 4. Create a user interface for your Jukebox so that the          user can to
+		 * 4. Create a user interface for your Jukebox so that the user can to
 		 * choose which song to play. You can use can use a different button for
 		 * each song, or a picture of the album cover. When the button or album
 		 * cover is clicked, stop the currently playing song, and play the one
 		 * that was selected.
 		 */
-    	
+    	JFrame frame = new JFrame();
+    	JButton button1 = new JButton("Myron - Lil Uzi Vert");
+    	JButton button2 = new JButton("Love Sosa - Chief Keef");
+    	JButton button3 = new JButton("Who Dat Boy - Tyler, The Creator");
     	JPanel panel = new JPanel();
-    	JLabel label = new JLabel();
+    	frame.add(panel);
+    	panel.add(button1);
+    	panel.add(button2);
+    	panel.add(button3);
+    	button1.add(loadImage("th.jpg"));
+    	button2.add(loadImage("Lovesosa.jpg"));
+    	button3.add(loadImage("WhoDatBoy911.jpg"));
+    	frame.pack();
+    	frame.setVisible(true);
+    	frame.setSize(1075,400);
+    		
     	
     }
     
